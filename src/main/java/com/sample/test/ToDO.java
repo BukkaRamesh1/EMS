d31ae655303c1e1f8d6d52cb1007dd1845dcf6e5
BukkaRamesh <bukkaramesh.b@gmail.com>
2022-07-14 09:40:34 -0400

    added package

---
+++ src/main/java/com/sample/test/ToDO.java
@@ -0,0 +1,48 @@
+package com.sample.test;
+
+
+/*
+ * 
+ *  Install java 8 
+ *  Install Eclipse IDE
+ *  
+ *  GitBash
+ *  
+ *  Open terminal and type a command   
+ *  1. java
+ *  2. javac
+ *  
+ *  if you get any error in javac like you path needs to be set to environment variables
+ *  
+ *  
+ *  JAVA_HOME = C:\Program Files\Java\jdk1.7.0_09\
+    PATH=C:\Program Files\Java\jdk1.7.0_09\bin;
+ * 
+ * 
+ *  git clone https://github.com/BukkaRamesh1/EMS.git
+ *  git checkout feature/name (creating our own branch)
+ *  git fetch
+ *  git status (shows you the all changes)
+ *  git add --filename (individual files to be added)
+ *  git add --all (adds all your files in a single shot)
+ *  git commit -m "commit message"
+ *  git push -u origin feature/name
+ *  use username and password is token 
+ *  
+ *  
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+public class ToDO {
+	
+	
+	
+
+}
